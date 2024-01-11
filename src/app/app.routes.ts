a7dbca23fb7e93aafa4659e7652bb641c875c29c
mohumadkhald <mohumadkhald@gmail.com>
2024-01-11 12:56:06 +0200

    app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'header', component: HeaderComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    // { path: '**', redirectTo:  }
+];
